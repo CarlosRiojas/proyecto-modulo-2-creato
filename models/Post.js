961b0a70418bee09820e290fac29f1ea5ed4de9c
Maria Reyna <mereynag@gmail.com>
2020-11-16 13:16:20 -0400

    User model and post model created

---
+++ models/Post.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const postSchema = new Schema({
+    ownerID: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: [Animals, Art, Cars, Crafts, Design, Food, Sports, Other]
+    },
+    content: String,
+    media: { 
+      type: String,
+    }
+}, {
+    timestamps: true
+})
+
+
+module.exports = model("Post", postSchema)
--- models/User.js
+++ models/User.js
@@ -1,15 +1,22 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const { Schema, model } = require('mongoose')
 
 const userSchema = new Schema({
-  username: String,
-  password: String
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: String,
+    role: {
+        type: String,
+        enum: ['COLLABORATOR', 'USER'],
+        default: 'USER'
+    },
+    picture: { type: String, default: 'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png' },
+    googleID: String,
 }, {
-  timestamps: {
-    createdAt: 'created_at',
-    updatedAt: 'updated_at'
-  }
-});
+    timestamps: true
+})
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+
+module.exports = model("User", userSchema)

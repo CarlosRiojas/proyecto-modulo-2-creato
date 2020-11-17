92e77c50623069f360e01ec734ff430640f1f97e
Maria Reyna <mereynag@gmail.com>
2020-11-17 12:24:30 -0400

    userPosts controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,8 +5,16 @@ exports.viewCreatePost = (req, res) => res.render('/createPost')
 exports.createPost = async(req, res) => {
     const { title, category, content } = req.body
     const media = req.file.path
-    await Post.create({ title, category, content, media })
+    await Post.create({ title, category, content, media, ownerID:user._id })
     res.render('/userPosts')
 }
 
 exports.userPosts = (req, res) => res.render('/userPosts')
+
+exports.userPosts = async (req, res) => {
+  const { user } = req
+  const posts = await Post.find({
+    ownerID: user._id
+  })
+  res.render("/userPosts", { posts })
+}
